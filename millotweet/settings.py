347f1d605ebd91b84c041befc2583d8ca7e048cb
millovanovic <bluesboba@gmail.com>
2018-09-19 15:50:54 +0200

    orchestration added

--- millotweet/settings.py
+++ millotweet/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'py)nz-exne*)0#f%gzt_rl_@&mp0aljagwh3xw3a$dh)5@838y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
---
+++ orchestration/main_vm.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+echo "I'm alive!"
+
+mkdir ~/millo
+sudo mkdir /mnt/tweets
+sudo mount /dev/vdb1 /mnt/tweets
+sudo chown ubuntu:ubuntu /mnt/tweets
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+echo "Installing pip..."
+sudo apt-get install -y python-pip
+sudo -H pip install --upgrade pip
+
+echo "Installing rabbitmq..."
+sudo apt-get install -y rabbitmq-server
+sudo service rabbitmq-server restart
+
+echo "Installing celery..."
+sudo -H pip install celery
+
+# echo "Installing flask..."
+# sudo -H pip install Flask
+
+echo "Installing django..."
+sudo -H pip install django
+
+echo "Installing milo..."
+cd /home/ubuntu
+sudo git clone https://github.com/millovanovic/acc-c3.git
+cd /home/ubuntu/acc-c3/
+sudo git checkout django
+python manage.py migrate
+# python manage.py runserver
+# echo "Running django..."
+
+
+echo "Initialization complete!"
